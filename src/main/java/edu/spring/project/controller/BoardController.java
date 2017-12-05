3e16c4650275ace4fd9e266173c340fd1b94f0d4
msk1145 <rail_moon@naver.com>
2017-12-05 19:56:59 +0900

    내꺼

---
+++ src/main/java/edu/spring/project/controller/BoardController.java
@@ -0,0 +1,41 @@
+package edu.spring.project.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.spring.project.service.BoardContentsService;
+import edu.spring.project.service.BoardFreeService;
+
+@Controller
+@RequestMapping(value = "/board")
+public class BoardController {
+	public static final Logger logger =
+			LoggerFactory.getLogger(BoardController.class);
+	
+	@Autowired
+	BoardContentsService boardConService;
+	@Autowired
+	BoardFreeService boardFreeService;
+	
+	@RequestMapping(value = "/boarddetail",
+			method = RequestMethod.GET)
+	public void detail() {
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
