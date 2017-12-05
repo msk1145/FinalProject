4c8f91e3adeee72b2de90e0bd972076f721bc955
spuups <spuups@naver.com>
2017-12-05 18:36:42 +0900

    TODO 2개

--- src/main/java/edu/spring/project/controller/MemberController.java
+++ src/main/java/edu/spring/project/controller/MemberController.java
@@ -24,9 +24,10 @@ public class MemberController {
 		
 		Member member = service.login(userid, password);
 		if(member == null) {// 로그인 실패
-			
+			// TODO: 실패 처리
 		}else { // 로그인 성공
 			m.addAttribute("member", member);
+			// TODO: ...
 		}
 		
 		// TODO: REST에서 로그인 전 화면으로 보내줘야함 
